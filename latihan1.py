f4eafdbbfd4795c5c57f5b5e51ccb4a3ee651d31
poppyharandtika <56400553+poppyharandtika@users.noreply.github.com>
2019-11-15 05:11:20 -0800

    Add files via upload

---
+++ latihan1.py
@@ -0,0 +1,10 @@
+
+
+
+
+from random import random
+num = int(input("Masukkan Bilangan N : "))
+for i in range(num):
+    x = random()
+    print("Perulangan Ke: {0} => {1}".format(i, x))
+print("Selesai")
---
+++ latihan2.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+
+terbesar = []
+while True:
+    n = int(input ("Masukkan Bilangan N: "))
+    terbesar.append(n)
+    if n == 0:
+        break
+print("Nilai Terbesar adalah : {}".format(max(terbesar)))
+
